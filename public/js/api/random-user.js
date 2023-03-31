f8e18a86396afd875ece02977e8303164bde65a1
serogbp <49165676+serogbp@users.noreply.github.com>
2023-03-31 08:43:11 +0200

    Pantalla Feed - Cargar posts mediante APIs

--- public/js/api/random-user.js
+++ public/js/api/random-user.js
@@ -3,3 +3,9 @@ export const getRandomUser = async () => {
 	const { results } = await result.json();
 	return results[0];
 };
+
+export const getRandomUsers = async (amount) => {
+	const result = await fetch(`https://randomuser.me/api?results=${amount}`);
+	const { results } = await result.json();
+	return results;
+};
