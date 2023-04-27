d715957c767630695542ac66755967cd6b039e5b
serogbp <49165676+serogbp@users.noreply.github.com>
2023-04-27 18:44:00 +0200

    Mostrar avatar del usuario logeado en el elemento de creación de post

--- Frontend/public/js/view/feed/createPost.js
+++ Frontend/public/js/view/feed/createPost.js
@@ -1,12 +1,14 @@
 const POST_CREATOR = document.getElementById("post-creator");
 const MAX_CHARACTERS = 280;
 
-const createElement = () => {
+const createElement = async () => {
+	const picture = await getPicture();
+
 	const element = document.createElement("div");
 	element.classList.add("d-flex", "flex-column", "gap-4", "card");
 	element.innerHTML = `
 		<div class="d-flex gap-3 align-items-center">
-			<img class="avatar" src="https://img.freepik.com/psd-premium/ilustracion-3d-hombre-caucasico-sonriente-retrato-cerca-dibujos-animados-hombre-caucasico-pie-sobre-fondo-amarillo-avatar-3d-ui-ux_1020-5081.jpg" alt="Imagen del usuario"/>
+			<img class="avatar" src="${picture}" alt="Imagen del usuario"/>
 			<p class="font-bold h5">Publicar un nuevo post</p>
 		</div>
 		<textarea id="create-post" name="create-post" autocomplete="off" placeholder="¿Qué está pasando?" class="p-3 rounded" style="height:100px;"></textarea>
@@ -69,7 +71,16 @@ const createElement = () => {
 	return element;
 };
 
-export const renderElement = () => {
-	const element = createElement();
+export const renderElement = async () => {
+	const element = await createElement();
 	POST_CREATOR.appendChild(element);
+};
+
+const getPicture = async () => {
+	const user_id = localStorage.getItem("idLogged");
+	const response = await fetch(`http://localhost:3000/user/${user_id}`, {
+		method: "GET",
+	});
+	const json = await response.json();
+	return json.picture;
 };
