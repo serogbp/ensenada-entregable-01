1fb07a027901146c4f0621782d50d8780396ec26
serogbp <49165676+serogbp@users.noreply.github.com>
2023-04-28 10:54:20 +0200

    Registro: Mostrar error usuario ya existe

--- Backend/Services/register.service.js
+++ Backend/Services/register.service.js
@@ -2,7 +2,6 @@ import { validationResult } from "express-validator";
 import { User } from "../Database/Models/User.js";
 import { connect } from "../Database/mysql.js";
 
-
 export const register = async (request, response) => {
 	const validationResults = validationResult(request);
 	if (!validationResults.isEmpty()) {
@@ -19,8 +18,13 @@ export const register = async (request, response) => {
 			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )`,
 			[user.name, user.surname1, user.surname2, user.username, user.email, user.password, user.age, user.city, user.country, user.studies, user.languages, user.linkedin, user.hobbies, user.role, user.picture]
 		);
+		return response.sendStatus(200);
 	} catch (error) {
+		if (error.code === "ER_DUP_ENTRY") {
+			const field = /^.*\'(\w+)\'$/.exec(error.message)[1];
+
+			return response.status(409).json({ msg: `Ya existe un usuario registrado con el ${field} introducido` });
+		}
 		return response.status(500).json({ msg: "No se ha podido registrar. Por favor, vuelva ha intentarlo mas tarde." });
 	}
-	return response.sendStatus(200);
 };
