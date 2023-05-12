f6eecf3fe20200f4a12c75a2687b604eace2a36b
serogbp <49165676+serogbp@users.noreply.github.com>
2023-05-12 19:21:22 +0200

    Pruebas en PerfilUsuario

--- Front_React/src/views/PerfilUsuario.jsx
+++ Front_React/src/views/PerfilUsuario.jsx
@@ -1,10 +1,36 @@
 import PerfilCabecera from "../components/PerfilCabecera";
-import MainLayout from "../layouts/Layout";
+import Prueba from "../components/profile/prueba";
+import MainLayout from "../layouts/MainLayout";
 
 export default function PerfilUsuario() {
+	// codigo que recoge datos del usuario
+	const user = {};
 	return (
 		<MainLayout>
-			<PerfilCabecera />
+			<div className="bg-light">
+				<PerfilCabecera />
+				<div className="p-4 d-flex gap-4 flex-column">
+					<Prueba
+						title={"Datos personales"}
+						campos={[
+							{ nombre: "Nombre", valor: "Juan" },
+							{ nombre: "Apellido", valor: "Perez" },
+							{ nombre: "Email", valor: "Juan@gmail" },
+							{ nombre: "Teléfono", valor: "123456789" },
+						]}
+					/>
+
+					<Prueba
+						title={"Formación académica"}
+						campos={[
+							{ nombre: "Nombre", valor: "Juan" },
+							{ nombre: "Apellido", valor: "Perez" },
+							{ nombre: "Email", valor: "Juan@gmail" },
+							{ nombre: "Teléfono", valor: "123456789" },
+						]}
+					/>
+				</div>
+			</div>
 		</MainLayout>
 	);
 }
