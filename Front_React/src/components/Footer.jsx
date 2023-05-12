06e32aa3cf8b9bd0be6a2606735ce40af17404a7
serogbp <49165676+serogbp@users.noreply.github.com>
2023-05-12 15:28:43 +0200

    Refactorizar CSS

--- Front_React/src/components/Footer.jsx
+++ Front_React/src/components/Footer.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
 	return (
 		<footer className="d-flex p-4 bg-black text-white">
-			<p className="h4 font-family--jetbrains-mono">
+			<Link to="/" className="fs-4 navbar-brand font-family--jetbrains-mono">
 				<strong>//TECLA</strong>_SOCIAL
-			</p>
+			</Link>
 		</footer>
 	);
 }
--- Front_React/src/components/Header.jsx
+++ Front_React/src/components/Header.jsx
@@ -1,12 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
 	return (
 		<header>
-			<nav className="navbar navbar-expand-lg bg-white">
-				<div className="container-fluid">
-					<a className="navbar-brand font-family--jetbrains-mono">
-						<strong>//TECLA</strong>_SOCIAL
-					</a>
-				</div>
+			<nav className="navbar p-4 navbar-expand-lg bg-white">
+				<Link to="/" className="fs-3 navbar-brand font-family--jetbrains-mono">
+					<strong>//TECLA</strong>_SOCIAL
+				</Link>
 			</nav>
 		</header>
 	);
