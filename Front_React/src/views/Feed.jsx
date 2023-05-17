cdca7a9ae7378eba06e6e174fd0427776a26ad2e
serogbp <49165676+serogbp@users.noreply.github.com>
2023-05-17 22:13:07 +0200

    Feed responsive

--- Front_React/src/views/Feed.jsx
+++ Front_React/src/views/Feed.jsx
@@ -1,5 +1,4 @@
 import FriendRequestList from "../components/FriendRequestList";
-import PostCreator from "../components/PostCreator";
 import PostList from "../components/PostList";
 import SideBarLinks from "../components/SideBarLinks";
 import MainLayout from "../layouts/MainLayout";
@@ -8,14 +7,14 @@ export default function Feed() {
 	return (
 		<MainLayout>
 			<div className="container">
-				<div className="row">
-					<div className="col ">
+				<div className="row gap-4">
+					<div className="col-lg d-none d-lg-block">
 						<SideBarLinks />
 					</div>
-					<div className="col-6 d-flex flex-column gap-4">
+					<div className="col-lg-6 d-flex flex-column gap-4 order-2">
 						<PostList />
 					</div>
-					<div className="col">
+					<div className="col-lg order-1 order-lg-3">
 						<FriendRequestList />
 					</div>
 				</div>
