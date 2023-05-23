d72156eb8d605e70fe45c7e649597bff9266fbf5
dxd-as <wilox_ryu@yahoo.es>
2023-05-23 23:23:39 +0200

    corregir class

--- Front_React/src/components/CreateFriendElement.jsx
+++ Front_React/src/components/CreateFriendElement.jsx
@@ -4,15 +4,13 @@ export default function CreateFriendElement(props) {
 	const { friendData } = props;
 	return (
 		<Link to={`/profile/${friendData.user_id}`} className="friend-card card d-flex flex-column gap-2 align-items-center" style={{ width: "2rem" }}>
-			<img src={friendData.picture} alt={`${friendData.name} ${friendData.surname1} ${friendData.surname2}`} class="avatar" />
-			<div class="doble-texto">
-				<p class="text-break">
+			<img src={friendData.picture} alt={`${friendData.name} ${friendData.surname1} ${friendData.surname2}`} className="avatar" />
+			<div className="doble-texto">
+				<p className="text-break">
 					{friendData.name} {friendData.surname1} {friendData.surname2}
 				</p>
-				<p class="text-break">@{friendData.username}</p>
+				<p className="text-break">@{friendData.username}</p>
 			</div>
 		</Link>
 	);
 }
-
-/* element.href = `/pages/perfil-de-usuario.html?user_id=${friendData.user_id}`; */
