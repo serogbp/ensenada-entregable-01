030c6cc05f297cb3d43c6d552af2b8ca3b4a7584
serogbp <49165676+serogbp@users.noreply.github.com>
2023-05-25 19:03:09 +0200

    Eventos clickables

--- Front_React/src/components/EventList.jsx
+++ Front_React/src/components/EventList.jsx
@@ -6,29 +6,32 @@ export default function EventList() {
 			date: moment("2023-05-19"),
 			body: "Weekly",
 			detail: "Sprint 02",
+			url: "https://docs.google.com/presentation/d/1Qp_f-k1RBdu-enZg3cjQg46ezIqRX7lQ/edit",
 		},
 		{
 			date: moment("2023-06-02"),
 			body: "Entrega final",
 			detail: "Sprint 02",
+			url: "https://docs.google.com/document/d/1iqK9n9YOgIWaR2KlP_jg6-JKGdfVvvz_/edit",
 		},
 	];
 	return (
 		<div className="card gap-4 bg-light-subtle">
 			<p className="h5 mb-1 font-family--jetbrains-mono">Eventos</p>
 			{events.map((event, index) => (
-				<EventListItem date={event.date} body={event.body} detail={event.detail} key={index} />
+				<EventListItem event={event} key={index} />
 			))}
 		</div>
 	);
 }
 
-function EventListItem({ date, body, detail }) {
+function EventListItem({ event }) {
+	const { date, body, detail, url } = event;
 	// Cambiar color del evento según cuántos días faltan
 	const diff = moment(date).diff(moment(), "days");
 	const bg = diff > 3 ? "bg-success" : "bg-danger";
 	return (
-		<div className="d-flex gap-2">
+		<a href={url} target="_blank" rel="noreferrer noopener" className="d-flex gap-2">
 			<div className={`event ${bg}`}>
 				<p className="h5">{moment(date).format("MMM")}</p>
 				<p className="h4">{moment(date).format("DD")}</p>
@@ -37,6 +40,6 @@ function EventListItem({ date, body, detail }) {
 				<strong>{body}</strong>
 				<p>{detail}</p>
 			</div>
-		</div>
+		</a>
 	);
 }
