7d54fc45f7df9826275a50cc963d62bf80a9a468
serogbp <49165676+serogbp@users.noreply.github.com>
2023-05-25 18:59:43 +0200

    Fix: Scroll on top cada vez que navegamos a una ruta

--- Front_React/src/layouts/MainLayout.jsx
+++ Front_React/src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 /*Layout para perfil, modificar perfil, feedm etc*/
 
+import ScrollToTop from "../common/ScrollOnTop";
 import BottomNavbar from "../components/BottomNavbar";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
@@ -7,6 +8,7 @@ import Navbar from "../components/Navbar";
 export default function MainLayout(props) {
 	return (
 		<div className="d-flex flex-column min-vh-100 bg-body-tertiary" id="main-layout">
+			<ScrollToTop />
 			<Navbar />
 			<main className="py-4 d-flex flex-column flex-grow-1">{props.children}</main>
 			<Footer />
