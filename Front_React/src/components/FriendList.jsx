140ef6a5cf84b52315ef0aa9de2eabd85d95f267
serogbp <49165676+serogbp@users.noreply.github.com>
2023-05-30 17:22:57 +0200

    Ordenar lista de amigos

--- Front_React/src/components/FriendList.jsx
+++ Front_React/src/components/FriendList.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react";
 import CreateFriendElement from "./CreateFriendElement";
 
+const SORT_TYPE = Object.freeze({
+	MANUAL: "manual",
+	NAME: "name",
+	USERNAME: "username",
+});
+
 export default function FriendList() {
 	const [friendList, setFriendList] = useState([]);
+	const [sortType, setSortType] = useState("manual");
+
 	useEffect(() => {
 		fetch(`http://localhost:3000/user/friends`, {
 			method: "GET",
@@ -21,20 +29,31 @@ export default function FriendList() {
 			});
 	}, []);
 
+	const sortList = () => {
+		switch (sortType) {
+			case SORT_TYPE.NAME:
+				return [...friendList].sort((a, b) => a.name.localeCompare(b.name));
+			case SORT_TYPE.USERNAME:
+				return [...friendList].sort((a, b) => a.username.localeCompare(b.username));
+			default:
+				return friendList;
+		}
+	};
+
 	return (
 		<>
 			<div className="d-flex flex-column gap-3">
 				<div className="d-flex gap-3 align-items-center">
 					<label htmlFor="sort-type">Ordenar por: </label>
-					<select id="sort-type" name="sort-type" className="rounded p-1">
-						<option value="manual">Orden de llegada</option>
+					<select id="sort-type" name="sort-type" className="rounded p-1" onChange={(e) => setSortType(e.target.value)}>
+						<option value="manual">Por defecto</option>
 						<option value="name">Nombre</option>
 						<option value="username">Username</option>
 					</select>
 				</div>
 				<p className="h3 mb-1 font-family--jetbrains-mono ">Mis amigos</p>
 				<div id="friend-list-wrapper" className="d-flex flex-wrap gap-3">
-					{friendList.map((friendData, index) => {
+					{sortList().map((friendData, index) => {
 						return <CreateFriendElement friendData={friendData} key={index} />;
 					})}
 				</div>
