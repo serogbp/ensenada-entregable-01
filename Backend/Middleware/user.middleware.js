66afa23f470895b8930b7e7c9fa89448322920ec
serogbp <49165676+serogbp@users.noreply.github.com>
2023-06-02 19:57:33 +0200

    Borrar codigo comentado

--- Backend/Middleware/user.middleware.js
+++ Backend/Middleware/user.middleware.js
@@ -25,23 +25,3 @@ export const checkUser = [
 		next();
 	},
 ];
-
-// export function checkUser(req, res, next) {
-// 	body("email").isEmail().escape();
-// 	body("password").isLength({ max: 10 }).escape();
-// 	body("name").isLength({ max: 150 }).escape();
-// 	body("surname1").isLength({ max: 150 }).escape();
-// 	body("surname2").isLength({ max: 150 }).escape();
-// 	body("email").isLength({ max: 150 }).escape();
-// 	body("password").isLength({ max: 150 }).escape();
-// 	body("age").isLength({ max: 3 }).escape();
-// 	body("city").isLength({ max: 150 }).escape();
-// 	body("country").isLength({ max: 150 }).escape();
-// 	body("studies").isLength({ max: 150 }).escape();
-// 	body("languages").isLength({ max: 150 }).escape();
-// 	body("linkedin").isLength({ max: 150 });
-// 	body("hobbies").isLength({ max: 150 }).escape();
-// 	body("role").isLength({ max: 150 }).escape();
-// 	body("picture").isLength({ max: 150 }).escape();
-// 	next();
-// }
